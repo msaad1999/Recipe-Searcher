30172e59ce2199536d9408eb7d163303475ca139
Muhammad Saad <saad01.1999@gmail.com>
2020-04-12 21:27:19 +0500

    setup API for searching

---
+++ models/api.js
@@ -0,0 +1,13 @@
+const API_CALL = "https://api.edamam.com/search";
+const APP_ID = "707b0925";
+const APP_KEY = "56c065dbb92734af43a553f50c3b0b97";
+
+function search(query) {
+    var url =
+        API_CALL + "?app_id=" + APP_ID + "&app_key=" + APP_KEY + "&q=" + query;
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
