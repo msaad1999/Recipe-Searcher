9282a1c0b577c01e2084a555cff05dafeef11102
Muhammad Saad <saad01.1999@gmail.com>
2020-04-24 15:50:12 +0500

    setup code for adding recipe to favorites

---
+++ models/favorites.js
@@ -0,0 +1,24 @@
+class Favorites {
+    constructor(uri, label, image, url) {
+        this.uri = uri;
+        this.label = label;
+        this.image = image;
+        this.url = url;
+    }
+
+    getUri() {
+        return this.uri;
+    }
+
+    getLabel() {
+        return this.label;
+    }
+
+    getImage() {
+        return this.image;
+    }
+
+    getUrl() {
+        return this.url;
+    }
+}
